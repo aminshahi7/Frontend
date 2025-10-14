ec496775837deaf6c09b9d41c2120a76bf783c21
Amin <amin@gmail.com>
2025-10-14 13:10:09 +0200

    first

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from 'react';
 import DatePicker from "react-datepicker";
 import 'react-datepicker/dist/react-datepicker.css';
 import Header from "../component/Header"
